307f4868d52cb9431d2a59c6326383540bd3a4ba
zero value <333zzz1@proton.me>
2026-09-24 20:23:59 +0300

    final decision

--- golang_web_services_2024-04-26/2/99_hw/signer/signer.go
+++ golang_web_services_2024-04-26/2/99_hw/signer/signer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -28,63 +29,82 @@ func ExecutePipeline(jobs ...job) {
 	wg.Wait()
 }
 
-func SingleHash(in, out chan interface{}) {
-	// inputData := []int{0, 1}
-	data := make([]int, 0)
+func SingleHash(in chan interface{}, out chan interface{}) {
+	valuesWG := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
 
-	for i, val := range data {
-		newVal := strconv.Itoa(val)
+	for val := range in {
+		valuesWG.Add(1)
+		newVal := strconv.Itoa(val.(int))
+		go func(newVal string, val interface{}) {
+			defer valuesWG.Done()
+			var crc32Data string
+			crc32WG := &sync.WaitGroup{}
+			crc32WG.Add(1)
 
-		md5 := DataSignerMd5(newVal)
-		crc32Data := DataSignerCrc32(newVal)
-		crc32md5 := DataSignerCrc32(DataSignerMd5(newVal))
-		result := DataSignerCrc32(newVal) + "~" + DataSignerCrc32(DataSignerMd5(newVal))
+			go func() {
+				defer crc32WG.Done()
+				crc32Data = DataSignerCrc32(newVal)
+			}()
+			mu.Lock()
+			md5 := DataSignerMd5(newVal)
+			mu.Unlock()
+			crc32md5 := DataSignerCrc32(md5)
+			crc32WG.Wait()
+			result := crc32Data + "~" + crc32md5
 
-		fmt.Printf("%d SingleHash data %s\n", i, newVal)
-		fmt.Printf("%d SingleHash md5(data) %s", i, md5)
-		fmt.Printf("%d SingleHash crc32(md5(data)) %s", i, crc32md5)
-		fmt.Printf("%d SingleHash crc32(data) %s", i, crc32Data)
-		fmt.Printf("%d SingleHash result %s", i, result)
+			out <- result
+			fmt.Printf("%d SingleHash data %s\n", val, newVal)
+			mu.Lock()
+			fmt.Printf("%d SingleHash md5(data) %s\n", val, md5)
+			mu.Unlock()
+			fmt.Printf("%d SingleHash crc32(md5(data)) %s\n", val, crc32md5)
+			fmt.Printf("%d SingleHash crc32(data) %s\n", val, crc32Data)
+			fmt.Printf("%d SingleHash result %s\n", val, result)
+		}(newVal, val)
 	}
+	valuesWG.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
-	data := make([]int, 0)
-	th := []int{0, 1, 2, 3, 4, 5}
-	var multiHashResult strings.Builder
-	for _, val := range data {
-		newVal := strconv.Itoa(val)
-		result := DataSignerCrc32(newVal) + "~" + DataSignerCrc32(DataSignerMd5(newVal))
-		for _, thNum := range th {
-			thNumStr := strconv.Itoa(thNum)
-			crc32 := DataSignerCrc32(thNumStr + newVal)
-			fmt.Printf("%s MultiHash: crc32(th+step1)) %s %s\n", result, thNumStr, crc32)
-			multiHashResult.WriteString(crc32)
-		}
-		fmt.Printf("%s MultiHash result: %s", result, multiHashResult.String())
+	valuesWG := &sync.WaitGroup{}
+	for val := range in {
+		valuesWG.Add(1)
+		go func(newVal string) {
+			defer valuesWG.Done()
+			wg := &sync.WaitGroup{}
+			var multiHashResult strings.Builder
+			results := make([]string, 6)
+
+			for thNum := 0; thNum < 6; thNum++ {
+				wg.Add(1)
+				go func(thNum int, newVal string) {
+					defer wg.Done()
+					thNumStr := strconv.Itoa(thNum)
+					crc32 := DataSignerCrc32(thNumStr + newVal)
+					fmt.Printf("%s MultiHash: crc32(th+step1)) %s %s\n", newVal, thNumStr, crc32)
+					results[thNum] = crc32
+				}(thNum, newVal)
+			}
+			wg.Wait()
+			for thNum := 0; thNum < 6; thNum++ {
+				multiHashResult.WriteString(results[thNum])
+			}
+			out <- multiHashResult.String()
+			fmt.Printf("%s MultiHash result: %s\n", newVal, multiHashResult.String())
+		}(val.(string))
 	}
+	valuesWG.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
-
+	var interimResult []string
+	for val := range in {
+		newVal := val.(string)
+		interimResult = append(interimResult, newVal)
+	}
+	sort.Strings(interimResult)
+	finalResult := strings.Join(interimResult, "_")
+	out <- finalResult
+	fmt.Printf("CombineResults %s\n", finalResult)
 }
-
-/*func main() {
-
-	job1 := func(in, out chan interface{}) {
-		value := <-in
-		fmt.Println("job1 получила:", value)
-		result := value.(int) * 2
-		fmt.Println("job1 отправила:", result)
-		out <- result
-	}
-	job2 := func(in, out chan interface{}) {
-		value := <-in
-		fmt.Println("job2 получила:", value)
-		result := value.(int) + 10
-		fmt.Println("job2 отправила:", result)
-		out <- result
-	}
-	ExecutePipeline(job1, job2)
-time.Sleep(time.Second)
-}*/
